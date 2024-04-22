45d6836d6ee177b4f0a320c1a33ef34287d628cb
memo2586 <lzy259475@gmail.com>
2024-04-22 16:56:05 +0800

    Blending

--- learnOpenGL_4/main.cpp
+++ learnOpenGL_4/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -24,7 +25,7 @@ const unsigned int SCR_WIDTH = 1600;
 const unsigned int SCR_HEIGHT = 1200;
 
 // camera
-Camera camera(glm::vec3(0.f));
+Camera camera(glm::vec3(0.f), glm::vec2(SCR_WIDTH / 2.f, SCR_HEIGHT / 2.f));
 bool firstMouse = true;
 float lastXpos = SCR_WIDTH / 2.f;
 float lastYpos = SCR_HEIGHT / 2.f;
@@ -88,26 +89,43 @@ int main() {
 		aiProcess_SortByPType
 	);
 
+	vector<glm::vec3> vegetation;
+	vegetation.push_back(glm::vec3(-1.5f, 0.0f, -0.48f));
+	vegetation.push_back(glm::vec3(1.5f, 0.0f, 0.51f));
+	vegetation.push_back(glm::vec3(0.0f, 0.0f, 0.7f));
+	vegetation.push_back(glm::vec3(-0.3f, 0.0f, -2.3f));
+	vegetation.push_back(glm::vec3(0.5f, 0.0f, -0.6f));
+
 	Shader lightShader("shader/3.3.only_diff.vert", "shader/3.3.only_diff.frag");
 	Shader shader("shader/3.3.shader.vert", "shader/3.3.shader.frag");
 
-	Model* floor = new Model("room/floor.obj");
-	Model* couch = new Model("room/couch.obj");
-	Model* pointlight = new Model("pointlight/pointlight.obj");
+	Model* floor = new Model("model/room/floor.obj");
+	//Model* kusa = new Model("model/kusacube/kusacube.obj");
+	Model* blend_window = new Model("model/window/blend_window.obj");
+	Model* pointlight = new Model("model/pointlight/pointlight.obj");
 
 	glEnable(GL_DEPTH_TEST);
 	glm::mat4 model;
 	glm::mat4 normal;
 
 	// 控制变量
-	camera.position = glm::vec3(2.5f, 1.5f, -1.5f);
-	camera.front = glm::vec3(-.83f, -.34f, .45f);
+	camera.position = glm::vec3(-4.5f, .2f, 0.f);
+	camera.front = glm::vec3(1.f, 0.f, 0.f);
+	glm::vec3 pointlight_pos;
+
+	// 启用混合
+	glEnable(GL_BLEND);
+	// 设置混合函数
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	while (!glfwWindowShouldClose(window)) {
 		// timing
 		float curTime = static_cast<float>(glfwGetTime());
 		deltaTime = curTime - lastTime;
 		lastTime = curTime;
+
+		// position
+		pointlight_pos = glm::vec3(sin(curTime), sin(curTime) / 4.f + .3f, cos(curTime));
 
 		// input
 		keyboard.processInput(window, deltaTime);
@@ -117,9 +135,19 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// render
-		shader.use();
 		glm::mat4 projection = glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		glm::mat4 view = camera.getViewMatrix();
+		// model: light
+		lightShader.use();
+		lightShader.setMat4f("projection", 1, glm::value_ptr(projection));
+		lightShader.setMat4f("view", 1, glm::value_ptr(view));
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, pointlight_pos);
+		model = glm::scale(model, glm::vec3(.01f));
+		lightShader.setMat4f("model", 1, glm::value_ptr(model));
+		pointlight->Draw(lightShader);
+
+		shader.use();
 		shader.setMat4f("projection", 1, glm::value_ptr(projection));
 		shader.setMat4f("view", 1, glm::value_ptr(view));
 		shader.setVec3f("viewPos", camera.position);
@@ -127,23 +155,26 @@ int main() {
 
 		// lighting
 		shader.setBool("pointLight.enable", 1);
-		shader.setVec3f("pointLight.position", glm::vec3(1.f, 1.f, 0.f));
+		shader.setVec3f("pointLight.position", pointlight_pos);
 		shader.setVec3f("pointLight.ambient", .1f, .1f, .1f);
 		shader.setVec3f("pointLight.diffuse", .5f, .5f, .5f);
 		shader.setVec3f("pointLight.specular", 1.f, 1.f, 1.f);
 		shader.setFloat("pointLight.constant", 1.f);
-		shader.setFloat("pointLight.linear", .09f);
-		shader.setFloat("pointLight.quadratic", .032f);
+		shader.setFloat("pointLight.linear", .045f);
+		shader.setFloat("pointLight.quadratic", .0075f);
 
-		// model: couch
-		model = glm::mat4(1.0f);
-		normal = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(0.f));
-		model = glm::scale(model, glm::vec3(.5f));
-		normal = glm::transpose(glm::inverse(model));
-		shader.setMat4f("model", 1, glm::value_ptr(model));
-		shader.setMat4f("nrmMat", 1, glm::value_ptr(normal));
-		couch->Draw(shader);
+		// model: kusa
+		//for (int i = 0; i < vegetation.size(); i++) {
+		//	model = glm::mat4(1.0f);
+		//	normal = glm::mat4(1.0f);
+		//	model = glm::translate(model, vegetation[i]);
+		//	model = glm::scale(model, glm::vec3(.3f));
+		//	normal = glm::transpose(glm::inverse(model));
+		//	shader.setMat4f("model", 1, glm::value_ptr(model));
+		//	shader.setMat4f("nrmMat", 1, glm::value_ptr(normal));
+		//	kusa->Draw(shader);
+		//}
+
 		// model: floor
 		model = glm::mat4(1.f);
 		normal = glm::mat4(1.0f);
@@ -154,16 +185,17 @@ int main() {
 		shader.setMat4f("model", 1, glm::value_ptr(model));
 		shader.setMat4f("nrmMat", 1, glm::value_ptr(normal));
 		floor->Draw(shader);
-		
-		// model: light
-		lightShader.use();
-		lightShader.setMat4f("projection", 1, glm::value_ptr(projection));
-		lightShader.setMat4f("view", 1, glm::value_ptr(view));
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(1.f, 1.f, 0.f));
-		model = glm::scale(model, glm::vec3(.01f));
-		lightShader.setMat4f("model", 1, glm::value_ptr(model));
-		pointlight->Draw(lightShader);
+
+		// blend_window
+		model = glm::mat4(1.f);
+		normal = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.f, .25f, 0.f));
+		model = glm::scale(model, glm::vec3(.5f));
+		model = glm::rotate(model, glm::radians(90.f), glm::vec3(0.f, 0.f, 1.f));
+		normal = glm::transpose(glm::inverse(model));
+		shader.setMat4f("model", 1, glm::value_ptr(model));
+		shader.setMat4f("nrmMat", 1, glm::value_ptr(normal));
+		blend_window->Draw(shader);
 
 		//Imgui
 		ImGui_ImplOpenGL3_NewFrame();
@@ -185,7 +217,8 @@ int main() {
 		glfwPollEvents();
 	}
 
-	delete couch;
+	//delete kusa;
+	delete blend_window;
 	delete floor;
 	delete pointlight;
 	glDeleteProgram(shader.ID);
--- learnOpenGL_4/shader/3.3.shader.frag
+++ learnOpenGL_4/shader/3.3.shader.frag
@@ -52,18 +52,25 @@ uniform PointLight pointLight;
 vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
 vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 viewDir);
+// 稍微修改一下点光源的计算方法让其使用四通道纹理值进行计算
+vec4 CalcPointLightRBGA(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 
 void main(){
 
 	vec3 viewDir = normalize(viewPos - fragPos);
 	vec3 norm = normalize(normal);
 
-	vec3 result = vec3(0.f);
-	if(dirLight.enable) result += CalcDirLight(dirLight, norm, viewDir);
-	if(pointLight.enable) result += CalcPointLight(pointLight, norm, fragPos, viewDir);
-	if(spotLight.enable) result += CalcSpotLight(spotLight, norm, viewDir);
-	
-	fragColor = vec4(result, 1.f);
+	// 如果alpha值太低直接丢弃这个片段
+	vec4 texColor = texture(material.texture_diffuse1, texCoord);
+//	if(texColor.a < 0.1) discard;
+
+	vec4 result = vec4(0.f);
+	//if(dirLight.enable) result += CalcDirLight(dirLight, norm, viewDir);
+	if(pointLight.enable) result += CalcPointLightRBGA(pointLight, norm, fragPos, viewDir);
+	//if(spotLight.enable) result += CalcSpotLight(spotLight, norm, viewDir);
+	fragColor = result;
+
+	//fragColor = texture(material.texture_diffuse1, texCoord);
 }
 
 vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir){
@@ -93,6 +100,23 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir){
 	vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, texCoord)) * attenuation;
 	vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, texCoord)) * attenuation;
 	vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, texCoord)) * attenuation;
+
+	return ambient + diffuse + specular;
+}
+
+vec4 CalcPointLightRBGA(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir){
+	// calc diff & spec
+	vec3 lightDir = normalize(light.position - fragPos);
+	float diff = max(0.f, dot(normal, lightDir));
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(0.f, dot(reflectDir, viewDir)), material.shininess);
+	// 计算距离衰减
+	float distance = length(light.position - fragPos);
+	float attenuation = 1.f / (light.constant + light.linear * distance + light.quadratic * distance * distance);
+	
+	vec4 ambient = vec4(light.ambient, 1.f) * texture(material.texture_diffuse1, texCoord).rbga * attenuation;
+	vec4 diffuse = vec4(light.diffuse, 1.f) * diff * texture(material.texture_diffuse1, texCoord).rbga * attenuation;
+	vec4 specular = vec4(light.specular, 1.f) * spec * texture(material.texture_specular1, texCoord).rbga * attenuation;
 
 	return ambient + diffuse + specular;
 }
